34b55a39baa71b97bf4668c152aeb1abffe97141
Simon Mika <smika@hx.se>
2010-11-18 10:46:43 +0000

    Fixed error.

--- src/Kean.Extra.Log/Writer/Csv.cs
+++ src/Kean.Extra.Log/Writer/Csv.cs
@@ -31,9 +31,9 @@ namespace Kean.Extra.Log.Writer
 		public override Action<Error.IError> Open()
 		{
 			this.writer = new System.IO.StreamWriter(this.Filename, true);
-			this.writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", "Time", "Level", "Assembly", "Title", "Message", "File", "Line", "Column"); 
+			this.writer.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7}", "Time", "Level", "Assembly", "Title", "Message", "File", "Line", "Column"); 
 			return (Error.IError entry) => {
-			this.writer.WriteLine("{0},{1},{2},\"{3}\",\"{4}\",{5},{6},{7},{8}", 
+			this.writer.WriteLine("{0},{1},{2},\"{3}\",\"{4}\",{5},{6},{7}", 
 				entry.Time, 
 				entry.Level, 
 				entry.Assembly.FullName,
