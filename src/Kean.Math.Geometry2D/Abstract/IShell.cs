917009a0c163c82f0d184fc69a78c3c945dd13ef
Anders Frisk <andersfrisk77@gmail.com>
2011-03-25 11:15:47 +0000

    added shell to 2d geometry.

---
+++ src/Kean.Math.Geometry2D/Abstract/IShell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kean.Math.Geometry2D.Abstract
+{
+    public interface IShell<V>
+        where V : struct
+    {
+        V Left { get; }
+        V Right { get; }
+        V Top { get; }
+        V Bottom { get; }
+    }
+}
---
+++ src/Kean.Math.Geometry2D/Abstract/Shell.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Kean.Math.Geometry2D.Abstract
+{
+    public abstract class Shell<TransformType, TransformValue, ShellType, ShellValue, BoxType, BoxValue, PointType, PointValue, SizeType, SizeValue, R, V> :
+        IShell<V>
+        where ShellType : Shell<TransformType, TransformValue, ShellType, ShellValue, BoxType, BoxValue, PointType, PointValue, SizeType, SizeValue, R, V>, IShell<V>, new()
+        where ShellValue : struct, IShell<V>
+        where TransformType : Transform<TransformType, TransformValue, SizeType, SizeValue, R, V>, ITransform<V>, new()
+        where TransformValue : struct, ITransform<V>
+        where BoxType : Box<TransformType, TransformValue, BoxType, BoxValue, PointType, PointValue, SizeType, SizeValue, R, V>, IBox<PointValue, SizeValue, V>, new()
+        where BoxValue : struct, IBox<PointValue, SizeValue, V>
+        where PointType : Point<TransformType, TransformValue, PointType, PointValue, SizeType, SizeValue, R, V>, IPoint<V>, new()
+        where PointValue : struct, IPoint<V>, IVector<V>
+        where SizeType : Size<TransformType, TransformValue, SizeType, SizeValue, R, V>, ISize<V>, new()
+        where SizeValue : struct, ISize<V>, IVector<V>
+        where R : Kean.Math.Abstract<R, V>, new()
+        where V : struct
+    {
+        R left;
+        R right;
+        R top;
+        R bottom;
+        public V Left { get { return this.left; } }
+        public V Right { get { return this.right; } }
+        public V Top { get { return this.top; } }
+        public V Bottom { get { return this.bottom; } }
+        public abstract ShellValue Value { get; }
+
+        #region Constructors
+        protected Shell()
+        {
+        }
+        protected Shell(V left, V right, V top, V bottom)
+        {
+            this.left = (R)left;
+            this.right = (R)right;
+            this.top = (R)top;
+            this.bottom = (R)bottom;
+        }
+        #endregion
+        #region Comparison Operators
+        public static bool operator ==(Shell<TransformType, TransformValue, ShellType, ShellValue, BoxType, BoxValue, PointType, PointValue, SizeType, SizeValue, R, V> left, IShell<V> right)
+        {
+            return object.ReferenceEquals(left, right) ||
+                !object.ReferenceEquals(left, null) && !object.ReferenceEquals(right, null) &&
+                (R)left.Left == right.Left &&
+                (R)left.Right == right.Right &&
+                (R)left.Top == right.Top &&
+                (R)left.Bottom == right.Bottom;
+
+        }
+        public static bool operator !=(Shell<TransformType, TransformValue, ShellType, ShellValue, BoxType, BoxValue, PointType, PointValue, SizeType, SizeValue, R, V> left, IShell<V> right)
+        {
+            return !(left == right);
+        }
+        #endregion
+        #region Object Overrides
+        /// <summary>
+        /// Return true if this object and <paramref name="other">other</paramref> are equal.
+        /// </summary>
+        /// <param name="other">Object to compare with</param>
+        /// <returns>True if this object and <paramref name="other">other</paramref> are equal else false.</returns>
+        public override bool Equals(object other)
+        {
+            return (other is ShellType) && this.Equals(other as ShellType);
+        }
+        /// <summary>
+        /// Return true if this object and <paramref name="other">other</paramref> are equal.
+        /// </summary>
+        /// <param name="other">Object to compare with</param>
+        /// <returns>True if this object and <paramref name="other">other</paramref> are equal else false.</returns>
+        public bool Equals(ShellType other)
+        {
+            return this == other;
+        }
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>Hash code for this instance.</returns>
+        public override int GetHashCode()
+        {
+            return this.Left.GetHashCode() ^ this.Right.GetHashCode() ^ this.Top.GetHashCode() ^ this.Bottom.GetHashCode();
+        }
+        #endregion
+    }
+}
