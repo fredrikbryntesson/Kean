196d327c465e555004bf28092aad4b44e0c0e016
Simon Mika <smika@hx.se>
2011-08-26 06:21:20 +0000

    Added KEan.Xml.Dom.Test.

---
+++ src/Kean.Xml.Dom.Test/Open.cs
@@ -0,0 +1,34 @@
+// 
+//  Open.cs
+//  
+//  Author:
+//       smika <${AuthorEmail}>
+//  
+//  Copyright (c) 2011 smika
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using NUnit.Framework;
+namespace Kean.Xml.Dom.Test
+{
+	[TestFixture()]
+	public class Open
+	{
+		[Test()]
+		public void TestCase()
+		{
+		}
+	}
+}
+
