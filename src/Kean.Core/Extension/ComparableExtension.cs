0a2d11a4fa08e07138905980c0f37196c450b542
Simon Mika <smika@hx.se>
2011-09-01 15:21:24 +0000

    NotifyAttribute and extension to IComparable.

---
+++ src/Kean.Core/Extension/ComparableExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Kean.Core.Extension
+{
+	public static class ComparableExtension
+	{
+		public static Order CompareWith(this IComparable me, IComparable other)
+		{
+			int order = me.CompareTo(other);
+			return order > 0 ? Order.GreaterThan : order == 0 ? Order.Equal : Order.LessThan;
+		}
+	}
+}
