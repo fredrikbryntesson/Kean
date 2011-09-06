f770c5a97741d4f07eeb70be8f5db2a8e7b75939
Anders Frisk <andersfrisk77@gmail.com>
2011-09-06 13:01:05 +0000

    Added small test on Linebuffer.

--- src/Kean.Cli.LineBuffer.Test/Program.cs
+++ src/Kean.Cli.LineBuffer.Test/Program.cs
@@ -20,6 +20,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using IO = Kean.IO;
+using Kean.Core.Extension;
 
 namespace Kean.Cli.LineBuffer.Test
 {
@@ -28,7 +29,56 @@ namespace Kean.Cli.LineBuffer.Test
 		static void Main(string[] args)
 		{
 			Editor editor = new Editor(new IO.ConsoleStream() { LocalEcho = false }, Console.Out);
-			editor.Read();
+            Kean.Core.Tuple<string, string>[] completionDescription = new Kean.Core.Tuple<string, string>[] 
+            {
+                Kean.Core.Tuple.Create<string, string>("fish", "Fishes are a paraphyletic group of organisms."),
+                Kean.Core.Tuple.Create<string, string>("firefox", "Mozilla Firefox is a free and open source web browser."),
+                Kean.Core.Tuple.Create<string, string>("foot", "The foot is an anatomical structure found in many vertebrates."),
+            };
+           
+            editor.Complete = text =>
+            {
+                string result = "";
+                string[] parts = text.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string last = parts[parts.Length - 1];
+                int hits = 0;
+                foreach (Kean.Core.Tuple<string, string> word in completionDescription)
+                    if (word.Item1.StartsWith(last))
+                    {
+                        result = word.Item1;
+                        hits++;
+                    }
+                if (hits == 0 || hits > 1)
+                    result = text;
+                else
+                    result = text.Remove(text.Length - last.Length) + result;
+                return result;
+            };
+            Kean.Core.Collection.IDictionary<string, Action> commands = new Kean.Core.Collection.Dictionary<string, Action>();
+            commands["play"] = () => Console.WriteLine("play");
+            commands["beep"] = () => Console.Beep();
+            commands["now"] = () => Console.WriteLine("now");
+            editor.Execute = text => 
+            {
+                string[] parts = text.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                bool correct = true;
+                foreach (string part in parts)
+                    correct &= commands.Contains(part);
+                if (correct)
+                    foreach (string part in parts)
+                        commands[part]();
+            };
+            editor.Help = text =>
+            {
+                System.Text.StringBuilder result = new System.Text.StringBuilder();
+                string[] parts = text.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                string last = parts[parts.Length - 1];
+                foreach (Kean.Core.Tuple<string, string> word in completionDescription)
+                    if (word.Item1.StartsWith(last))
+                        result.AppendLine(word.Item1 + " " + word.Item2);
+                return result.ToString();   
+            };
+            editor.Read();
 		}
 	}
 }
