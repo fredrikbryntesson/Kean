f6655c1cea6ae864fbc2e80fc7861449493a603e
Simon Mika <smika@hx.se>
2011-09-19 08:15:19 +0000

    Added Kean.IO.Tap.

---
+++ src/Kean.IO.Tap/ByteDevice.cs
@@ -0,0 +1,99 @@
+﻿// 
+//  ByteDevice.cs
+//  
+//  Author:
+//       Simon Mika <smika@hx.se>
+//  
+//  Copyright (c) 2011 Simon Mika
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Kean.Core;
+using Kean.Core.Extension;
+using Collection = Kean.Core.Collection;
+using Kean.Core.Collection.Extension;
+
+namespace Kean.IO
+{
+	public class ByteDevice :
+		IByteDevice
+	{
+		IByteDevice backend;
+		public event Action<byte> OnRead;
+		public event Action<byte> OnWrite;
+
+		#region Constructors
+		public ByteDevice(IByteDevice backend, Action<byte> onRead, Action<byte> onWrite) :
+			this(backend)
+		{
+			this.OnRead = onRead;
+			this.OnWrite = onWrite;
+		}
+		public ByteDevice(IByteDevice backend)
+		{
+			this.backend = backend;
+		}
+		#endregion
+		#region IByteDevice Members
+		public bool Readable { get { return this.backend.NotNull() && this.backend.Readable; } }
+		public bool Writeable { get { return this.backend.NotNull() && this.backend.Writeable; } }
+		#endregion
+		byte? Convert(int value)
+		{
+			return value < 0 ? null : (byte?)value;
+		}
+		#region IByteInDevice Members
+		public byte? Peek()
+		{
+			return this.backend.Peek();
+		}
+		public byte? Read()
+		{
+			byte? result = this.backend.Read();
+			if (result.HasValue)
+				this.OnRead.Call(result.Value);
+			return result;
+		}
+		#endregion
+		#region IByteOutDevice Members
+		public bool Write(System.Collections.Generic.IEnumerable<byte> buffer)
+		{
+			return this.backend.Write(buffer.Cast(b => { this.OnWrite.Call(b); return b; }));
+		}
+		#endregion
+		#region IInDevice Members
+		public bool Empty { get { return this.backend.Empty; } }
+		#endregion
+		#region IDevice Members
+		public virtual bool Opened { get { return this.backend.NotNull() && this.backend.Opened; } }
+		public virtual bool Close()
+		{
+			bool result;
+			if (result = this.backend.NotNull())
+			{
+				this.backend.Close();
+				this.backend = null;
+			}
+			return result;
+		}
+		#endregion
+		#region IDisposable Members
+		void IDisposable.Dispose()
+		{
+			this.Close();
+		}
+		#endregion
+	}
+}
---
+++ src/Kean.IO.Tap/CharacterDevice.cs
@@ -0,0 +1,96 @@
+﻿// 
+//  CharacterDevice.cs
+//  
+//  Author:
+//       Simon Mika <smika@hx.se>
+//  
+//  Copyright (c) 2011 Simon Mika
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Kean.Core;
+using Kean.Core.Extension;
+using Collection = Kean.Core.Collection;
+using Kean.Core.Collection.Extension;
+
+namespace Kean.IO.Tap
+{
+	public class CharacterDevice :
+		ICharacterDevice
+	{
+		ICharacterDevice backend;
+		public event Action<char> OnRead;
+		public event Action<char> OnWrite;
+
+		#region Constructors
+		public CharacterDevice(ICharacterDevice backend, Action<char> onRead, Action<char> onWrite) :
+			this(backend)
+		{
+			this.OnRead = onRead;
+			this.OnWrite = onWrite;
+		}
+		public CharacterDevice(ICharacterDevice backend)
+		{
+			this.backend = backend;
+		}
+		~CharacterDevice()
+		{
+			this.Close();
+		}
+		#endregion
+		#region ICharacterDevice Members
+		public bool Readable { get { return this.backend.NotNull() && this.backend.Readable; } }
+		public bool Writeable { get { return this.backend.NotNull() && this.backend.Writeable; } }
+		#endregion
+		#region ICharacterOutDevice Members
+		public bool Write(System.Collections.Generic.IEnumerable<char> buffer)
+		{
+			return this.backend.Write(buffer.Cast(c => { this.OnWrite.Call(c); return c; }));
+		}
+		#endregion
+		#region ICharacterInDevice Members
+		public char? Peek()
+		{
+			return this.backend.Peek();
+		}
+		public char? Read()
+		{
+			char? result = this.backend.Read();
+			if (result.HasValue)
+				this.OnRead.Call(result.Value);
+			return result;
+		}
+		#endregion
+		#region IInDevice Members
+		public bool Empty { get { return this.backend.NotNull() && this.backend.Empty; } }
+		#endregion
+		#region IDevice Members
+		public virtual bool Opened { get { return this.backend.NotNull() && this.backend.Opened; } }
+		public virtual bool Close()
+		{
+			bool result;
+			if (result = this.backend.NotNull() && this.backend.Close())
+				this.backend = null;
+			return result;
+		}
+		#endregion
+		#region IDisposable Members
+		void IDisposable.Dispose()
+		{
+			this.Close();
+		}
+		#endregion
+
+	}
+}
