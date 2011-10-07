5e6200eca4c39474aa5be31dea6acdfb51c95ae0
Simon Mika <smika@hx.se>
2011-10-07 15:00:32 +0000

    Added Cairo and Pango and start of paths.

---
+++ src/Kean.Draw/Path.cs
@@ -0,0 +1,47 @@
+﻿// 
+//  Path.cs
+//  
+//  Author:
+//       Simon Mika <smika@hx.se>
+//  
+//  Copyright (c) 2011 Simon Mika
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Kean.Core;
+using Kean.Core.Extension;
+using Geometry2D = Kean.Math.Geometry2D;
+
+namespace Kean.Draw
+{
+	public class Path
+	{
+		public void MoveTo(Geometry2D.Single.Point end)
+		{
+		}
+		public void LineTo(Geometry2D.Single.Point end)
+		{
+		}
+		public void CurveTo(Geometry2D.Single.Point first, Geometry2D.Single.Point second, Geometry2D.Single.Point end)
+		{
+		}
+		public void EllipticalArcTo(Geometry2D.Single.Point radius, float angle, bool largeArc, bool sweep, Geometry2D.Single.Point end)
+		{
+		}
+ 		public void Close()
+		{
+		}
+	}
+}
---
+++ src/Kean.Draw/PathSegment/CurveTo.cs
@@ -0,0 +1,41 @@
+﻿// 
+//  CurveTo.cs
+//  
+//  Author:
+//       Simon Mika <smika@hx.se>
+//  
+//  Copyright (c) 2011 Simon Mika
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Kean.Core;
+using Kean.Core.Extension;
+using Geometry2D = Kean.Math.Geometry2D;
+
+namespace Kean.Draw.PathSegment
+{
+	public class CurveTo :
+		Abstract
+	{
+		public Geometry2D.Single.Point First { get; set; }
+		public Geometry2D.Single.Point Second { get; set; }
+		public CurveTo(Geometry2D.Single.Point first, Geometry2D.Single.Point second, Geometry2D.Single.Point end) :
+			base(end)
+		{
+			this.First = first;
+			this.Second = second;
+		}
+	}
+}
