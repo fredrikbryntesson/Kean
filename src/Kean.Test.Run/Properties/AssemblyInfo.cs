02ef87ccb602bb4de7baffbeb39baa161653acd5
Simon Mika <smika@hx.se>
2011-11-01 08:59:30 +0000

    Added test of assembly versioning from svn revision.

--- src/Kean.Test.Run/Properties/AssemblyInfo.cs
+++ src/Kean.Test.Run/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// 
+﻿// 
 //  AssemblyInfo.cs
 //  
 //  Author:
@@ -27,20 +27,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Kean.Test.Run")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
