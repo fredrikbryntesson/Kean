111baf3fbadf48e48f730cbc2fdd2775f2b3d535
Simon Mika <smika@hx.se>
2011-11-01 10:45:29 +0000

    Clean up.

--- src/Kean/CommonAssemblyInfo.cs
+++ src/Kean/CommonAssemblyInfo.cs
@@ -18,11 +18,9 @@
 // 
 //  You should have received data copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 using System.Reflection;
 using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes. 
-// Change them to the values specific to your project.
 
 [assembly: AssemblyCompany("Kean")]
 [assembly: AssemblyProduct("Kean Toolkit")]
@@ -30,14 +28,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
 [assembly: AssemblyVersion("0.2.0.0")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
