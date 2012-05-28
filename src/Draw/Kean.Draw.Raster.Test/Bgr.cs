1a9b343c04655dd92c49e9417bed85b8bc612f32
Simon Mika <smika@hx.se>
2012-05-28 06:05:35 +0000

    Added Bgr tests.

--- src/Draw/Kean.Draw.Raster.Test/Bgr.cs
+++ src/Draw/Kean.Draw.Raster.Test/Bgr.cs
@@ -1,10 +1,10 @@
 ﻿// 
-//  All.cs
+//  Bgr.cs
 //  
 //  Author:
-//       Anders Frisk <andersfrisk77@gmail.com>
+//      Simon Mika <smika@hx.se
 //  
-//  Copyright (c) 2011 Simon Mika
+//  Copyright (c) 2012 Simon Mika
 // 
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
@@ -20,17 +20,21 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using Geometry2D = Kean.Math.Geometry2D;
 
 namespace Kean.Draw.Raster.Test
 {
-	public static class All
+	public class Bgr :
+		Image<Bgr>
 	{
-		public static void Test()
+		public Bgr() :
+			base("Bgr", 0.01f)
+		{ }
+		protected override Draw.Image CreateImage(Geometry2D.Integer.Size size)
 		{
-			Convert.Test();
-			Transform.Test();
-			Bgra.Test();
-			Bgr.Test();
+			return new Raster.Bgr(size);
 		}
 	}
 }
