eb048ed182b6e95bf648875e071016e62967c533
Anders Frisk <andersfrisk77@gmail.com>
2012-05-29 11:06:14 +0000

    Added basic Buffer types.

---
+++ src/Core/Kean.Core.Buffer/Single.cs
@@ -0,0 +1,34 @@
+﻿// 
+//  Single.cs
+//  
+//  Author:
+//       Anders Frisk <andersfrisk77@gmail.com>
+//  
+//  Copyright (c) 2009-2012 Anders Frisk
+// 
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using Kean.Core.Extension;
+
+namespace Kean.Core.Buffer
+{
+	public class Single :
+		Vector<float>
+	{
+		public Single(int size) : this(size, null) { }
+		public Single(int size, Action<IntPtr> free) : base(size, free) { }
+		public Single(float[] data) : base(data, null) { }
+		public Single(float[] data, Action<IntPtr> free) : base(data, free) { }
+	}
+}
