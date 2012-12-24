c4c27ed0850292604225b9dbda7cfa08784ba81b
Simon Mika <smika@hx.se>
2012-12-24 22:10:21 +0000

    Fixed exception with logging after dispose of log module.

--- src/Kean/Platform/Kean.Platform.Log/Module.cs
+++ src/Kean/Platform/Kean.Platform.Log/Module.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       Simon Mika <smika@hx.se>
 //  
-//  Copyright (c) 2011 Simon Mika
+//  Copyright (c) 2011-2012 Simon Mika
 // 
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
@@ -114,6 +114,7 @@ namespace Kean.Platform.Log
 			{
 				if (this.SaveLog)
 					this.Flush();
+				Error.Log.OnAppend -= this.Append;
 				this.cache.Dispose();
 				this.cache = null;
 			}
