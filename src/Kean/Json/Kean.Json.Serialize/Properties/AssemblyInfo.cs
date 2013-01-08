fc4b38a91ea3b5a0622e4e13be2491942f3ac89c
Simon Mika <smika@hx.se>
2013-01-08 14:40:37 +0000

    Cleanup.

--- src/Kean/Json/Kean.Json.Serialize/Properties/AssemblyInfo.cs
+++ src/Kean/Json/Kean.Json.Serialize/Properties/AssemblyInfo.cs
@@ -24,24 +24,5 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Kean.Json.Dom.Test")]
+[assembly: AssemblyTitle("Kean.Json.Serialize")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Cogneco")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Simon Mika")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly, 
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
