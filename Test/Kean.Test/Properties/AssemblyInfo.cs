80b79e97bb42317d01f6b44c04f0a184881b207f
Simon Mika <smika@hx.se>
2013-03-27 13:31:39 +0100

    Added building of Nuget package of Kean.Test.

--- Test/Kean.Test/Properties/AssemblyInfo.cs
+++ Test/Kean.Test/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kean.Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base classes for unit testing fixtures.")]
 [assembly: AssemblyConfiguration("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
