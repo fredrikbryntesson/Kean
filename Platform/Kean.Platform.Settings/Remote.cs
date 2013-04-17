1e4357185b8af80381592882be53f0be8d1599dd
Simon Mika <smika@hx.se>
2013-04-17 08:08:35 -0700

    Added support for feedback on method calls to remote client.

--- Platform/Kean.Platform.Settings/Remote.cs
+++ Platform/Kean.Platform.Settings/Remote.cs
@@ -4,15 +4,24 @@ using IO = Kean.IO;
 using Kean.Core.Extension;
 using Parallel = Kean.Core.Parallel;
 using Collection = Kean.Core.Collection;
+using Error = Kean.Core.Error;
+using Kean.Core;
 
 namespace Kean.Platform.Settings
 {
 	public class Remote :
+		Synchronized,
 		IDisposable
 	{
 		Collection.IDictionary<string, Action<string>> values = new Collection.Synchronized.Dictionary<string, Action<string>>();
 		Collection.IDictionary<string, Action<string>> notifications = new Collection.Synchronized.Dictionary<string, Action<string>>();
-
+		object onResponseLock = new object();
+		Action<bool> onResponse;
+		event Action<bool> OnResponse
+		{
+			add { lock(this.onResponseLock) this.onResponse += value; }
+			remove { lock (this.onResponseLock) this.onResponse -= value; }
+		}
 		IO.ICharacterReader reader;
 		IO.ICharacterWriter writer;
 		Parallel.RepeatThread thread;
@@ -36,9 +45,46 @@ namespace Kean.Platform.Settings
 		{
 			return true;
 		}
-		public void Call(string method, params object[] arguments)
+		public bool Call(string method, params object[] arguments)
 		{
+			object @lock = new object();
+			bool done = false;
+			bool result = false;
+			Action<string> previous = this.values[method];
+			Action<bool> onResponse = success =>
+			{
+				lock (@lock)
+				{
+					result = success;
+					done = true;
+					this.values[method] = previous;
+					System.Threading.Monitor.PulseAll(@lock);
+				}
+			};
+			this.values[method] = value =>
+			{
+				lock (@lock)
+				{
+					if (value == "failed")
+					{
+						result = false;
+						this.OnResponse -= onResponse;
+						done = true;
+						System.Threading.Monitor.PulseAll(@lock);
+					}
+				}
+				this.values.Remove(method);
+				previous.Call(value);
+			};
+			this.OnResponse += onResponse;
 			this.Send(method, arguments);
+			if (this.thread.IsCurrent)
+				this.Receive();
+			else
+				lock (@lock)
+					while (!done)
+						System.Threading.Monitor.Wait(@lock, 500);
+			return result;
 		}
 		public T Get<T>(string property)
 		{
@@ -53,7 +99,6 @@ namespace Kean.Platform.Settings
 			this.Send(property + " notify");
 			this.notifications[property] = value => callback(value.Parse<T>());
 		}
-
 		T Receive<T>(string property, Action send)
 		{
 			object @lock = new object();
@@ -86,35 +131,56 @@ namespace Kean.Platform.Settings
 
 		void Receive()
 		{
-			IO.Text.Builder line = null;
-			while (this.reader.Next() && this.reader.Last != '\n')
+			lock (this.Lock)
 			{
-				if (line.NotNull() || !char.IsWhiteSpace(this.reader.Last))
-					if (this.reader.Last == '>' && line == "# ")
-						line = null;
-					else if (!char.IsControl(this.reader.Last))
-						line += this.reader.Last;
-			}
-			Console.WriteLine("< " + line);
-			string[] splitted = ((string)line).Split(new char[] { ' ' }, 3);
-			if (splitted.Length > 1)
-			{
-				switch (splitted[0])
+				IO.Text.Builder line = null;
+				while (this.reader.Next() && this.reader.Last != '\n')
 				{
-					case "$": // value
-						if (splitted.Length > 2)
-							this.values[splitted[1]].Call(splitted[2]);
-						break;
-					case "%": // notification
-						if (splitted.Length > 2)
-							this.notifications[splitted[1]].Call(splitted[2]);
-						break;
-					default:
-					case "!": // error
-						System.Diagnostics.Debug.WriteLine(line);
-						break;
+					if (line.NotNull() || !char.IsWhiteSpace(this.reader.Last))
+						if (this.reader.Last == '>' && line == "# ")
+						{
+							line = null;
+							this.OnResponseCall(true);
+						}
+						else if (!char.IsControl(this.reader.Last))
+							line += this.reader.Last;
+				}
+				Console.WriteLine("< " + line);
+				string[] splitted = ((string)line).Split(new char[] { ' ' }, 3);
+				if (splitted.Length > 1)
+				{
+					switch (splitted[0])
+					{
+						case "$": // value
+							if (splitted.Length > 2)
+							{
+								this.values[splitted[1]].Call(splitted[2]);
+								this.OnResponseCall(true);
+							}
+							break;
+						case "%": // notification
+							if (splitted.Length > 2)
+								this.notifications[splitted[1]].Call(splitted[2]);
+							break;
+						default:
+						case "!": // error
+							this.OnResponseCall(false);
+							System.Diagnostics.Debug.WriteLine(line);
+							break;
+					}
 				}
 			}
+		}
+
+		void OnResponseCall(bool success)
+		{
+			Action<bool> onResponse;
+			lock (this.onResponseLock)
+			{
+				onResponse = this.onResponse;
+				this.onResponse = null;
+			}
+			onResponse.Call(success);
 		}
 
 		void Send(string message, params object[] arguments)
