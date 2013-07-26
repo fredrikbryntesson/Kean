7fd66964ae3ccef005aab12489a0da7319136ea0
Simon Mika <smika@hx.se>
2013-07-26 17:31:05 +0200

    JSON serialization storage compiles.

--- Json/Kean.Json.Serialize/Storage.cs
+++ Json/Kean.Json.Serialize/Storage.cs
@@ -32,91 +32,154 @@ namespace Kean.Json.Serialize
 	public class Storage : 
 		Core.Serialize.Storage
 	{
-		public Storage () :
+		public Storage() :
 			base()
 		{
 		}
-		public Storage (Core.Serialize.IRebuilder rebuilder, params Core.Serialize.ISerializer[] serializers) :
+		public Storage(Core.Serialize.IRebuilder rebuilder, params Core.Serialize.ISerializer[] serializers) :
 			base(rebuilder, serializers)
 		{
 		}
-		protected override Core.Serialize.Data.Node Load (Uri.Locator resource)
+		protected override Core.Serialize.Data.Node Load(Uri.Locator resource)
 		{
-			Dom.Item root = Dom.Item.Open (resource);
-			return root.NotNull () ? this.Convert (root) : null;
+			Dom.Item root = Dom.Item.Open(resource);
+			return root.NotNull() ? this.Convert(root) : null;
 		}
-		string GetTextContent (Dom.Element element)
+		Core.Serialize.Data.Node Convert(Dom.Item item)
 		{
-			return element.Fold ((n, s) => s + (n is Dom.Text ? (n as Dom.Text).Value : n is Dom.Data ? (n as Dom.Data).Value : ""), "");
-		}
-		Core.Serialize.Data.Node Convert (Dom.Item item)
-		{
-			Core.Serialize.Data.Node result = null;
-			if (item.NotNull ())
+			Core.Serialize.Data.Node result;
+			if (item is Dom.Object)
+				result = this.Convert(item as Dom.Object);
+			else if (item is Dom.Primitive)
 			{
-				Dom.Attribute type = item.Attributes.Find (a => a.Name == "type");
-				if (type.NotNull () && type.Value == "link")
-					result = new Core.Serialize.Data.Link (this.GetTextContent (item)) {
-						Name = item.Name,
-						Region = item.Region
-					};
+				if (item is Dom.String)
+					result = new Kean.Core.Serialize.Data.String((item as Dom.String).Value);
+				else if (item is Dom.Number)
+					result = new Kean.Core.Serialize.Data.Decimal((item as Dom.Number).Value);
+				else if (item is Dom.Boolean)
+					result = new Kean.Core.Serialize.Data.Boolean((item as Dom.Boolean).Value);
 				else
-				if (item.All (n => !(n is Dom.Element)))
-					result = new Core.Serialize.Data.String (this.GetTextContent (item)) {
-						Name = item.Name,
-						Region = item.Region
-					};
-				else
-				{
-					result = new Core.Serialize.Data.Branch () { Name = item.Name, Region = item.Region };
-					foreach (Dom.Node node in item)
-						if (node is Dom.Element)
-							(result as Core.Serialize.Data.Branch).Nodes.Add (this.Convert (node as Dom.Element));
-				}
-				if (type.NotNull ())
-					result.Type = type.Value;
+					result = null;
+			}
+			else if (item is Dom.Array)
+				result = this.Convert(item as Dom.Array);
+			else
+				result = null;
+			return result;
+		}
+		Core.Serialize.Data.Branch Convert(Dom.Object item)
+		{
+			Core.Serialize.Data.Branch result = new Core.Serialize.Data.Branch() { Region = item.Region };
+			foreach (KeyValue<Json.Dom.Label, Dom.Item> child in item)
+			{
+				Core.Serialize.Data.Node c = this.Convert(child.Value);
+				c.Name = child.Key;
+				result.Nodes.Add(c);
 			}
 			return result;
 		}
-		protected override bool Store (Core.Serialize.Data.Node value, Uri.Locator resource)
+		Core.Serialize.Data.Collection Convert(Dom.Array item)
 		{
-			return new Dom.Document () { Root = this.Convert(value) }.Save (resource);
-		}
-		Dom.Element Convert (Core.Serialize.Data.Node element)
-		{
-			Dom.Element result = null;
-			if (element.NotNull ())
-			{
-				result = new Dom.Element (element.Name ?? "node");
-				if (element is Core.Serialize.Data.Link)
-				{
-					result.Attributes.Add (new Kean.Xml.Dom.Attribute ("type", "link"));
-					result.Add (new Dom.Text ((element as Core.Serialize.Data.Link).Relative));
-				}
-				else
-				{
-					if (element.Type.NotNull ())
-						result.Attributes.Add (new Kean.Xml.Dom.Attribute ("type", element.Type));
-					if (element is Core.Serialize.Data.Leaf)
-						result.Add (new Dom.Text ((element as Core.Serialize.Data.Leaf).Text));
-					else
-					if (element is Core.Serialize.Data.Branch)
-						(element as Core.Serialize.Data.Branch).Nodes.Apply (e => result.Add (this.Convert (e)));
-				}
-			}
+			Core.Serialize.Data.Collection result = new Core.Serialize.Data.Collection() { Region = item.Region };
+			foreach (Dom.Item child in item)
+				result.Nodes.Add(this.Convert(child));
 			return result;
 		}
-		public static T Open<T> (Uri.Locator resource)
+		protected override bool Store(Core.Serialize.Data.Node value, Uri.Locator resource)
 		{
-			return new Storage ().Load<T> (resource);
+			return this.Convert(value).Save(resource);
 		}
-		public static bool Save<T> (T value, Uri.Locator resource)
+		Dom.Item Convert(Core.Serialize.Data.Node item)
 		{
-			return new Storage ().Store<T> (value, resource);
+			Dom.Item result;
+			if (item is Core.Serialize.Data.Branch)
+				result = this.Convert(item as Core.Serialize.Data.Branch);
+			else if (item is Core.Serialize.Data.Leaf)
+				result = this.Convert(item as Core.Serialize.Data.Leaf);
+			else if (item is Core.Serialize.Data.Collection)
+				result = this.Convert(item as Core.Serialize.Data.Collection);
+			else if (item is Core.Serialize.Data.Link)
+				result = this.Convert(item as Core.Serialize.Data.Link);
+			else
+				result = new Kean.Json.Dom.Null(item.Region);
+			return result;
 		}
-		public static bool Save<T> (T value, Uri.Locator resource, string name)
+		Dom.Object Convert(Core.Serialize.Data.Branch item)
 		{
-			return new Storage ().Store<T> (value, resource, name);
+			Dom.Object result = new Dom.Object(item.Region);
+			if (item.Type.NotNull())
+				result.Add("_type", (string)item.Type);
+			item.Nodes.Apply(e => result.Add(e.Name, this.Convert(e)));
+			return result;
+		}
+		Dom.Primitive Convert(Core.Serialize.Data.Leaf item)
+		{
+			Dom.Primitive result;
+			if (item is Core.Serialize.Data.Binary)
+				result = new Dom.String((item as Core.Serialize.Data.Binary).Text);
+			else if (item is Core.Serialize.Data.Boolean)
+				result = new Dom.Boolean((item as Core.Serialize.Data.Boolean).Value);
+			else if (item is Core.Serialize.Data.Byte)
+				result = new Dom.Number((item as Core.Serialize.Data.Byte).Value);
+			else if (item is Core.Serialize.Data.Character)
+				result = new Dom.String((item as Core.Serialize.Data.Character).Text);
+			else if (item is Core.Serialize.Data.DateTime)
+				result = new Dom.String((item as Core.Serialize.Data.DateTime).Text);
+			else if (item is Core.Serialize.Data.DateTimeOffset)
+				result = new Dom.String((item as Core.Serialize.Data.DateTimeOffset).Text);
+			else if (item is Core.Serialize.Data.Decimal)
+				result = new Dom.Number((item as Core.Serialize.Data.Decimal).Value);
+			else if (item is Core.Serialize.Data.Double)
+				result = new Dom.Number((Decimal)(item as Core.Serialize.Data.Double).Value);
+			else if (item is Core.Serialize.Data.Enumeration)
+				result = new Dom.String((item as Core.Serialize.Data.Enumeration).Text);
+			else if (item is Core.Serialize.Data.Integer)
+				result = new Dom.Number((item as Core.Serialize.Data.Integer).Value);
+			else if (item is Core.Serialize.Data.Long)
+				result = new Dom.Number((item as Core.Serialize.Data.Long).Value);
+			else if (item is Core.Serialize.Data.Short)
+				result = new Dom.Number((item as Core.Serialize.Data.Short).Value);
+			else if (item is Core.Serialize.Data.SignedByte)
+				result = new Dom.Number((item as Core.Serialize.Data.SignedByte).Value);
+			else if (item is Core.Serialize.Data.Single)
+				result = new Dom.Number((Decimal)(item as Core.Serialize.Data.Single).Value);
+			else if (item is Core.Serialize.Data.String)
+				result = new Dom.String((item as Core.Serialize.Data.String).Value);
+			else if (item is Core.Serialize.Data.TimeSpan)
+				result = new Dom.String((item as Core.Serialize.Data.TimeSpan).Text);
+			else if (item is Core.Serialize.Data.UnsignedInteger)
+				result = new Dom.Number((item as Core.Serialize.Data.UnsignedInteger).Value);
+			else if (item is Core.Serialize.Data.UnsignedLong)
+				result = new Dom.Number((item as Core.Serialize.Data.UnsignedLong).Value);
+			else if (item is Core.Serialize.Data.UnsignedShort)
+				result = new Dom.Number((item as Core.Serialize.Data.UnsignedShort).Value);
+			else
+				result = new Kean.Json.Dom.Null();
+			return result;
+		}
+		Dom.Array Convert(Core.Serialize.Data.Collection item)
+		{
+			Dom.Array result = new Dom.Array(item.Region);
+			item.Nodes.Apply(e => result.Add(this.Convert(e)));
+			return result;
+		}
+		Dom.Object Convert(Core.Serialize.Data.Link item)
+		{
+			Dom.Object result = new Dom.Object(item.Region);
+			result.Add("_link", new Dom.String(item.Locator, item.Region));
+			return result;
+		}
+		public static T Open<T>(Uri.Locator resource)
+		{
+			return new Storage().Load<T>(resource);
+		}
+		public static bool Save<T>(T value, Uri.Locator resource)
+		{
+			return new Storage().Store<T>(value, resource);
+		}
+		public static bool Save<T>(T value, Uri.Locator resource, string name)
+		{
+			return new Storage().Store<T>(value, resource, name);
 		}
 	}
 }
