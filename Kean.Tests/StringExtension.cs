091be36aeacd27b4cac84bf0c5840b8779027edb
Simon Mika <smika@hx.se>
2013-09-05 09:27:27 +0200

    Builds in place.

--- Kean.Tests/StringExtension.cs
+++ Kean.Tests/StringExtension.cs
@@ -1,48 +1,46 @@
-﻿// 
+﻿//
 //  StringExtension.cs
-//  
+//
 //  Author:
 //       Simon Mika <smika@hx.se>
-//  
+//
 //  Copyright (c) 2010 Simon Mika
-// 
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
-// 
+//
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Lesser General Public License for more details.
-// 
+//
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
 using NUnit.Framework;
 using Kean.Extension;
-
 namespace Kean.Test
 {
-	[TestFixture]
-	public class StringExtension :
+    [TestFixture]
+    public class StringExtension :
 		Kean.Test.Fixture<StringExtension>
-	{
-		protected override void Run()
-		{
-			this.Run(this.Split);
-		}
-		[Test]
-		public void Split()
-		{
-			Verify("hello\"good bye".SplitAt(), Is.EqualTo(new string[] { "hellogood bye" }));
-			Verify("\"hello goodbye\"".SplitAt(), Is.EqualTo(new string[] { "hello goodbye" }));
-			Verify("hello goodbye".SplitAt(), Is.EqualTo(new string[] { "hello", "goodbye" }));
-			Verify("hello\\tgoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\tgoodbye" }));
-			Verify("hello\\r\\ngoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\r\ngoodbye" }));
-			Verify("hello\\bgoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\bgoodbye" }));
-			Verify("hello\" hello\"goodbye".SplitAt(), Is.EqualTo(new string[] { "hello hellogoodbye" }));
-		}
-	}
+    {
+        protected override void Run()
+        {
+            this.Run(this.Split);
+        }
+        [Test]
+        public void Split()
+        {
+            Verify("hello\"good bye".SplitAt(), Is.EqualTo(new string[] { "hellogood bye" }));
+            Verify("\"hello goodbye\"".SplitAt(), Is.EqualTo(new string[] { "hello goodbye" }));
+            Verify("hello goodbye".SplitAt(), Is.EqualTo(new string[] { "hello", "goodbye" }));
+            Verify("hello\\tgoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\tgoodbye" }));
+            Verify("hello\\r\\ngoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\r\ngoodbye" }));
+            Verify("hello\\bgoodbye".SplitAt(), Is.EqualTo(new string[] { "hello\bgoodbye" }));
+            Verify("hello\" hello\"goodbye".SplitAt(), Is.EqualTo(new string[] { "hello hellogoodbye" }));
+        }
+    }
 }
