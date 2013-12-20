d6626ea5dd341dd07de68b59818a682130d69e12
David Hesselbom <david.hesselbom@imint.se>
2013-12-20 15:44:26 +0100

    Commented out version info.

--- Kean/Properties/AssemblyInfo.cs
+++ Kean/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.CompilerServices;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("0.2.*")]
+//[assembly: AssemblyVersion("0.2.*")]
 //[assembly: AssemblyVersion("0.0.0.0")]
 //[assembly: AssemblyInformationalVersion("0.2.*")]
 //[assembly: AssemblyFileVersion("")]
